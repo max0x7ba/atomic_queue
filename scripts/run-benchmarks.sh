7b9380dc5b4057fb4e62d8c68d62dec43159b9d2
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2019-06-03 22:27:31 +0100

    A script added.

---
+++ scripts/run-benchmarks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+now=$(date --utc +%Y%m%dT%H%M%S)
+cpucount=$(grep -c ^processor /proc/cpuinfo)
+exe="$(dirname "$0")/../benchmarks"
+
+function benchmark() {
+    lb="/usr/bin/stdbuf -oL"
+    ((N=33))
+    for((i=1;i<=N;++i)); do
+        $lb echo -n "[$i/$N] "
+        sudo chrt -f 50 "$exe"
+    done
+}
+
+benchmark |& tee results-${cpucount}.${now}.txt
