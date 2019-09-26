3cbab14a1d966569ca218bc29bdfe998cc8386d7
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2019-09-26 23:40:37 +0100

    Another benchmarking script added.

---
+++ scripts/run-benchmarks-for.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Copyright (c) 2019 Maxim Egorushkin. MIT License. See the full licence in file LICENSE.
+
+lb="/usr/bin/stdbuf --output=L"
+
+minutes=${1:-10}
+$lb echo "Running benchmarks for ${minutes} minutes."
+end_time=$(($(date +%s) + minutes * 60))
+
+now=$(date --utc +%Y%m%dT%H%M%S)
+cpucount=$(grep -c ^processor /proc/cpuinfo)
+exe="$(dirname "$0")/../benchmarks"
+
+function benchmark() {
+    i=0
+    while((1)); do
+        remaining=$((end_time - $(date +%s)))
+        ((remaining <= 0)) && break
+        ((++i))
+        $lb echo "[$i] $((remaining / 60))m:$((remaining % 60))s to go..."
+        sudo chrt -f 50 "$exe"
+    done
+}
+
+benchmark | tee results-${cpucount}.${now}.txt
