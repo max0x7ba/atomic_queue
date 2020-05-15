4d208ee06c2cc092b6b2ae5a7b7fe813a8d495c7
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2020-05-15 17:19:41 +0100

    Disable real-time thread throttling when benchmarking.

--- scripts/benchmark-prologue.sh
+++ scripts/benchmark-prologue.sh
@@ -5,3 +5,11 @@ set +e # Ignore failures.
 sudo hugeadm --pool-pages-min 1GB:1 --pool-pages-max 1GB:1
 sudo cpupower frequency-set --related --governor performance >/dev/null
 ~/scripts/cpu-fans.sh 5 >/dev/null
+
+if [[ -e /proc/sys/kernel/sched_rt_runtime_us ]]; then
+    if [[ ! -e .sched_rt_runtime_us.txt ]]; then
+        cat /proc/sys/kernel/sched_rt_runtime_us >.sched_rt_runtime_us.txt~
+        mv -f .sched_rt_runtime_us.txt{~,}
+    fi
+    echo -1 | sudo tee /proc/sys/kernel/sched_rt_runtime_us >/dev/null
+fi
