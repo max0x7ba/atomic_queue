d5241602d0d83927c0532cc404a0f517023eb23b
Cameron <cameron@moodycamel.com>
2020-06-22 08:56:36 -0400

    Added consumer token use to moodycamel wrapper

--- include/atomic_queue/moodycamel.h
+++ include/atomic_queue/moodycamel.h
@@ -15,29 +15,53 @@ namespace atomic_queue {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-template<class T, unsigned Capacity, class Queue>
-struct MoodyCamelAdapter : Queue {
-    MoodyCamelAdapter()
-        : Queue(Capacity) {}
+template<class T, unsigned Capacity>
+struct MoodyCamelQueue : moodycamel::ConcurrentQueue<T> {
+    MoodyCamelQueue()
+        : moodycamel::ConcurrentQueue<T>(Capacity) {
+        ctok() = moodycamel::ConcurrentQueue<T>::consumer_token_t(*this);  // update instance
+    }
 
     void push(T element) {
-        while(!this->try_enqueue(element))
+        // TODO: To be able to use try_enqueue properly, particularly with tokens,
+        // requires the number of producer and consumer threads to be passed to the
+        // constructor
+        while (!this->try_enqueue(element))
             spin_loop_pause();
     }
 
     T pop() {
         T element;
-        while(!this->try_dequeue(element))
+        while (!this->try_dequeue(ctok(), element))
             spin_loop_pause();
         return element;
+    }
+
+private:
+    moodycamel::ConcurrentQueue<T>::consumer_token_t& ctok() {
+        // Assume just one instance is present at a time
+        static thread_local moodycamel::ConcurrentQueue<T>::consumer_token_t ctok(*this);
+        return ctok;
     }
 };
 
 template<class T, unsigned Capacity>
-using MoodyCamelQueue = MoodyCamelAdapter<T, Capacity, moodycamel::ConcurrentQueue<T>>;
+struct MoodyCamelReaderWriterQueue : moodycamel::ReaderWriterQueue<T> {
+    MoodyCamelReaderWriterQueue()
+        : moodycamel::ReaderWriterQueue<T>(Capacity) {}
 
-template<class T, unsigned Capacity>
-using MoodyCamelReaderWriterQueue = MoodyCamelAdapter<T, Capacity, moodycamel::ReaderWriterQueue<T>>;
+    void push(T element) {
+        while (!this->try_enqueue(element))
+            spin_loop_pause();
+    }
+
+    T pop() {
+        T element;
+        while (!this->try_dequeue(element))
+            spin_loop_pause();
+        return element;
+    }
+};
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
