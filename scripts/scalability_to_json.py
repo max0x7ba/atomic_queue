0680696c4d0b26d905bb0aa9887120d87c3be70d
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2020-07-05 21:44:09 +0100

    Display mean, stdev, min, max in the scalability tooltip.

--- scripts/scalability_to_json.py
+++ scripts/scalability_to_json.py
@@ -13,15 +13,12 @@ from parse_output import *
 results = list(parse_output(sys.stdin))
 df = as_scalability_df(results)
 
-columns = defaultdict(list) # name: [threads, mean].
-errorbars = defaultdict(list) # name: [threads, low, high].
+# name
+## thread, min, max, mean, stdev
+output3 = defaultdict(list)
 for (name, threads), data in df.groupby(['queue', 'threads']):
     s = data["msg/sec"].describe(percentiles=None)
     threads = int(threads)
-    columns[name].append([threads, int(s["mean"])])
-    errorbars[name].append([threads, int(s["min"]), int(s["max"])])
-
-output1 = [{"name" : n, "type": "column", "data": d} for n, d in columns.items()]
-output2 = [{"name" : n, "type": "errorbar", "data": d} for n, d in errorbars.items()]
-json.dump([j for i in zip(output1, output2) for j in i], sys.stdout)
+    output3[name].append([threads, *[int(s[f]) for f in ['min', 'max', 'mean', 'std']]])
+json.dump(output3, sys.stdout)
 print()
