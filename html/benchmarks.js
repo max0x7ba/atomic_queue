e515709058f75871461a4b9d9f01ae01863d9f93
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2022-07-23 17:25:29 +0100

    Use svg icon.

--- html/benchmarks.js
+++ html/benchmarks.js
@@ -170,16 +170,18 @@ $(function() {
     plot_latency('latency-xeon-gold-6132', latency_xeon_gold_6132);
     plot_latency('latency-ryzen-5950x', latency_ryzen_5950x);
 
-    $(".view-toggle").on("click", function() {
-        const toggle = $(this);
-        toggle.next().slideToggle();
-        toggle.children("span").each(function() {
-            $(this).animate(
-                { hidden: this.hidden ^ 1 },
-                { step: function(f) { $(this).css("transform", `rotate(${-90 * f}deg)`); } }
-            );
+    $(".view-toggle")
+        .html((index, html) => `<svg class="arrow-down-circle" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/></svg> ${html}`)
+        .on("click", function() {
+            const toggle = $(this);
+            toggle.next().slideToggle();
+            toggle.children("svg.arrow-down-circle").each(function() {
+                $(this).animate(
+                    { hidden: this.hidden ^ 1 },
+                    { step: function(f) { $(this).css("transform", `rotate(${-90 * f}deg)`); } }
+                );
+            });
         });
-    });
 
     $("body").css("visibility", "visible");
 });
