4561bc86cc3d10ba47bc76ceed4936fa7dc29370
Maxim Egorushkin <max@supernova2>
2025-10-10 03:10:02 +0100

    More robust real-time throttling restore when benchmarking.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,30 +1,38 @@
 #!/bin/bash
 
+set -e # Exit on errors.
+
+sched_rt=/proc/sys/kernel/sched_rt_runtime_us
+orig_sched_rt=/tmp/${sched_rt##*/}.txt
+
 function prologue {
-    set +e # Ignore failures.
+    sudo hugeadm --pool-pages-min 1GB:1 || : # Ignore failures.
+    sudo cpupower frequency-set --related --governor performance >/dev/null || : # Ignore failures.
 
-    sudo hugeadm --pool-pages-min 1GB:1
-    sudo cpupower frequency-set --related --governor performance >/dev/null
-
-    if [[ -e /proc/sys/kernel/sched_rt_runtime_us ]]; then
-        if [[ ! -e .sched_rt_runtime_us.txt ]]; then
-            cat /proc/sys/kernel/sched_rt_runtime_us >.sched_rt_runtime_us.txt~
-            mv -f .sched_rt_runtime_us.txt{~,}
-        fi
-        echo -1 | sudo tee /proc/sys/kernel/sched_rt_runtime_us >/dev/null
+    # Disable real-time throttling. Save its original value.
+    if [[ -e ${sched_rt} ]]; then
+        local tmp_orig_sched_rt=${orig_sched_rt}.${BASHPID}~
+        [[ -e ${orig_sched_rt} ]] || cat ${sched_rt} > ${tmp_orig_sched_rt}
+        echo -1 | {
+            sudo tee ${sched_rt} >/dev/null
+            [[ ! -e ${tmp_orig_sched_rt} ]] || mv -f ${tmp_orig_sched_rt} ${orig_sched_rt}
+        }
     fi
 }
 
 function epilogue {
-    set +e # Ignore failures.
-    sudo cpupower frequency-set --related --governor powersave >/dev/null
-    [[ -f .sched_rt_runtime_us.txt ]] && sudo tee /proc/sys/kernel/sched_rt_runtime_us >/dev/null <.sched_rt_runtime_us.txt
+    sudo cpupower frequency-set --related --governor powersave >/dev/null || : # Ignore failures.
+
+    # Restore original real-time throttling value.
+    if [[ -f ${orig_sched_rt} ]]; then
+        sudo tee ${sched_rt} >/dev/null <${orig_sched_rt} && rm ${orig_sched_rt}
+    fi
 }
 
 function benchmark {
-    trap "epilogue" EXIT
+    trap epilogue EXIT
     prologue
-    (set -e -x; exec "$@")
+    (set -e; exec "$@")
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
