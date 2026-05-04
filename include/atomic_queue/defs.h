4e1c8b9c1f79fdd29bc54e1f27d9045b799f25c4
Maxim Egorushkin <maxim.egorushkin@gmail.com>
2026-05-04 04:21:33 +0100

    Remove unnecessary pessimisation volatile qualifier from ARM spin_loop_pause implementations.

--- include/atomic_queue/defs.h
+++ include/atomic_queue/defs.h
@@ -47,18 +47,6 @@ ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {
 }
 
 #elif defined(__arm__) || defined(__aarch64__) || defined(_M_ARM64)
-/*
-TODO:
-
-"asm volatile" is adequate and sufficient to prevent the asm statements from being reordered by a compiler relative to any and all preceding and following statements.
-
-The "memory" clobber in an asm statement invalidates all memory and all registers loaded from memory prior to the asm statement, forcing compilers to emit otherwise unnecessary machine code to reload registers from memory. "memory" clobbers is one of the worst performance killers -- a key motivation for inventing std::memory_order. std::memory_order is the extreme opposite of asm "memory" clobbers enabling fine-grained control of non-atomic instruction reordering relative to atomic instructions, obviating the need to ever use the most detrimental and undesirable asm "memory" clobbers.
-
-The asm "memory" clobbers defeat and undo any and all positive effects of the precise weakest and cheapest possible std::memory_order arguments this library calls std::atomic member functions with. The benchmarks built for ARM are unlikely to perform anywhere near/similar to the x86_64 benchmark levels of performance with the asm "memory" clobber.
-
-The effects of asm "memory" clobber have only recently become intuitively familiar to me and I don't have access to a multi-core ARM workstation to benchmark the performance boost of removing the asm "memory" clobber. Which I expect to be significant, based on my experience of relaxing std::memory_orders on x86_64 platform. Hence, benchmarking on multi-core ARM hardware is required to validate/justify removing the "memory" clobber.
-*/
-
 constexpr int CACHE_LINE_SIZE = 64;
 ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {
 #if (defined(__ARM_ARCH_6K__) || \
@@ -72,18 +60,18 @@ ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {
      defined(__ARM_ARCH_7S__) || \
      defined(__ARM_ARCH_8A__) || \
      defined(__aarch64__))
-    asm volatile ("yield" ::: "memory");
+    asm("yield" ::: "memory");
 #elif defined(_M_ARM64)
     __yield();
 #else
-    asm volatile ("nop" ::: "memory");
+    asm("nop" ::: "memory");
 #endif
 }
 
 #elif defined(__ppc64__) || defined(__powerpc64__)
 constexpr int CACHE_LINE_SIZE = 128;
 ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {
-    asm volatile("or 31,31,31 # very low priority");
+    asm("or 31,31,31 # very low priority");
 }
 
 #elif defined(__s390x__)
@@ -93,13 +81,13 @@ ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {} // TODO: Find the 
 #elif defined(__riscv)
 constexpr int CACHE_LINE_SIZE = 64;
 ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {
-    asm volatile (".insn i 0x0F, 0, x0, x0, 0x010");
+    asm(".insn i 0x0F, 0, x0, x0, 0x010");
 }
 
 #elif defined(__loongarch__)
 constexpr int CACHE_LINE_SIZE = 64;
 ATOMIC_QUEUE_INLINE static void spin_loop_pause() noexcept {
-    asm volatile("nop \n nop \n nop \n nop \n nop \n nop \n nop \n nop");
+    asm("nop \n nop \n nop \n nop \n nop \n nop \n nop \n nop");
 }
 
 #else
